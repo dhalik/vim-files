40d2f3cdc1d097e8de7ea22b5060ce0098cc815c
kam <kamaljot.dhaliwal@raventelemetry.com>
2016-01-04 17:49:49 -0500

    updated dot files to not be hidden and updated install script to symlink

--- .zshrc
+++
@@ -1,2 +0,0 @@
-# Created by newuser for 4.3.17
-alias school='ssh -XC k4dhaliw@linux.student.cs.uwaterloo.ca'
--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
-sudo chsh -s /bin/zsh
-sudo apt-get install sshfs
-sudo apt-get install i3
+cp .tmux.conf ~/.tmux.conf
+cp .vimrc ~/.vimrc
+cp .xmobarrc ~/.xmobarrc
+cp -r .xmonad ~/.xmonad
+cp .bashrc ~/.bashrc
+ln -s tmux.conf ~/.tmux.conf
+ln -s vimrc ~/.vimrc
+ln -s xmobarrc ~/.xmobarrc
+ln -s bashrc ~/.bashrc
+ln -s xmonad ~/.xmonad
